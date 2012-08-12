2fe90bd5f65ad697478d4af3e3525b9768b455c1
Artem Sapegin <artem@sapegin.ru>
2012-08-13 00:08:47 +0400

    Rewritten gruntfile template.

--- grunt/tasks/init/gruntfile.js
+++ grunt/tasks/init/gruntfile.js
@@ -1,26 +1,15 @@
-/*
- * grunt
- * https://github.com/cowboy/grunt
- *
- * Copyright (c) 2012 "Cowboy" Ben Alman
- * Licensed under the MIT license.
- * http://benalman.com/about/license/
+/**
+ * Gruntfile template for Grunt
  */
 
-// Basic template description.
-exports.description = 'Create a basic grunt.js gruntfile.';
-
-// Template-specific notes to be displayed before question prompts.
-exports.notes = 'This template tries to guess file and directory paths, but ' +
-	'you will most likely need to edit the generated grunt.js file before ' +
-	'running grunt. _If you run grunt after generating grunt.js, and grunt ' +
-	'exits with errors, edit the grunt.js file!_';
-
-// Any existing file or directory matching this wildcard will cause a warning.
+exports.description = 'Creates a grunt.js gruntfile.';
 exports.warnOn = 'grunt.js';
 
 // The actual init template.
+/*jshint node:true, white:false, smarttabs:true, strict:false */
 exports.template = function(grunt, init, done) {
+	'use strict';
+
 	var fs = require('fs');
 
 	grunt.helper('prompt', {}, [
@@ -38,6 +27,12 @@ exports.template = function(grunt, init, done) {
 			warning: 'Yes: stylus task. No: nothing to see here.'
 		},
 		{
+			name: 'sweet',
+			message: 'Sweet?',
+			default: 'y/N',
+			warning: 'Yes: sweet task. No: nothing to see here.'
+		},
+		{
 			name: 'library',
 			message: 'Library name?',
 			default: 'none',
@@ -46,30 +41,189 @@ exports.template = function(grunt, init, done) {
 	], function(err, props) {
 		grunt.utils._.defaults(props, init.defaults);
 
-		props.js = /y/i.test(props.js);
-		props.stylus = /y/i.test(props.stylus);
-		props.package_json = fs.existsSync('package.json');
-		props.wordpress = fs.existsSync('header.php') && fs.existsSync('footer.php') && fs.existsSync('functions.php');
-		props.test_task = props.dom ? 'qunit' : 'test';
-		props.file_name = props.package_json ? '<%= pkg.name %>' : 'scripts';
+		var stylus = props.stylus = /y/i.test(props.stylus);
+		var sweet = props.sweet = !/n/i.test(props.sweet);
+		var library = props.library;
+		var js = /y/i.test(props.js);
+		var package_json = fs.existsSync('package.json');
+		var wordpress = fs.existsSync('header.php') && fs.existsSync('footer.php') && fs.existsSync('functions.php');
+		var js_script = package_json ? '<%= pkg.name %>' : 'scripts';
 
-		// Find the first `preferred` item existing in `arr`.
-		function prefer(arr, preferred) {
-			for (var i = 0; i < preferred.length; i++) {
-				if (arr.indexOf(preferred[i]) !== -1) {
-					return preferred[i];
+		// Guess at some directories, if they exist
+		var dirs = grunt.file.expandDirs('*').map(function(d) { return d.slice(0, -1); });
+		var htdocs_dir = prefer(dirs, ['htdocs', 'www']);
+		var htdocs_prefix = htdocs_dir ? htdocs_dir + '/' : '';
+		var lib_dir = prefer(dirs, [htdocs_prefix + 'js/mylibs', htdocs_prefix + 'lib', htdocs_prefix + 'src']);
+
+		// JS libraries
+		props.jquery = grunt.file.expandFiles('**/jquery*.js').length > 0;
+
+		// Config
+		var cfg = {};
+
+		if (js) {
+			if (package_json) {
+				cfg.pkg = '<json:package.json>';
+				cfg.meta = {
+					banner: [
+						'/*! <%= pkg.title || pkg.name %> v<%= pkg.version %>\n',
+						' * © <%= pkg.author.name %>, <%= pkg.homepage ? pkg.homepage : "" %>, ',
+							'<%= grunt.template.today("yyyy") %> - ',
+							' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+					].join('')
+				};
+			}
+			else {
+				cfg.meta = {};
+				if (library) {
+					if (!package_json) {
+						cfg.meta.version = '0.0.0';
+					}
+					cfg.meta.banner = [
+						'/*! ', library, ' v' , (package_json ? '<%= pkg.version %>' : '<%= meta.version %>'), '\n',
+						' * © ', props.author_name, ', ', props.author_url, ', <%= grunt.template.today("yyyy") %> - ',
+							'Licensed MIT */'
+					].join('');
+				}
+				else {
+					cfg.meta.banner = [
+						'/*! Author: ', props.author_name, ', ', props.author_url, ', ',
+							'<%= grunt.template.today("yyyy") %> */'
+					].join('');
 				}
 			}
-			return preferred[0];
+
+			cfg.lint = {
+				files: [
+					'grunt.js',
+					htdocs_prefix + 'js/main.js'
+				]
+			};
+			if (lib_dir) {
+				cfg.lint.files.push(lib_dir + '/*.js');
+			}
+
+			cfg.concat = {
+				dist: {
+					src: [
+						htdocs_prefix + 'js/utils.js',
+						htdocs_prefix + 'js/main.js'
+					],
+					dest: htdocs_prefix + 'build/' + js_script + '.js'
+				}
+			};
+
+			cfg.min = {
+				dist: {
+					src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+					dest: htdocs_prefix + 'build/' + js_script + '.min.js'
+				}
+			};
 		}
 
-		// Guess at some directories, if they exist.
-		var dirs = grunt.file.expandDirs('*').map(function(d) { return d.slice(0, -1); });
-		props.lib_dir = prefer(dirs, ['js/mylibs', 'lib', 'src']);
-		props.test_dir = prefer(dirs, ['test', 'tests', 'unit', 'spec']);
+		if (stylus) {
+			cfg.stylus = {
+				compile: {
+					files: {},
+					options: {
+						'include css': true,
+						'paths': ['styles']
+					}
+				}
+			};
 
-		// Maybe this should be extended to support more libraries. Patches welcome!
-		props.jquery = grunt.file.expandFiles('**/jquery*.js').length > 0;
+			cfg.stylus.compile.files[wordpress ? 'style.css' : htdocs_prefix + 'build/styles.css'] = 'styles/index.styl';
+		}
+
+		if (sweet) {
+			cfg.sweet = {
+				content_dir: 'content',
+				publish_dir: htdocs_dir || 'htdocs',
+				templates_dir: 'templates',
+				default_template_id: 'template',
+				langs: ['ru', 'en'],
+				url_prefixes: {
+					ru: '/ru/',
+					en: '/'
+				},
+				uri_prefixes: {
+					ru: '/ru/',
+					en: '/'
+				},
+				files: {
+					css: {
+						path: htdocs_prefix + 'build/styles.css',
+						href: '/' + htdocs_prefix + 'build/styles.css?{version}'
+					},
+					js: {
+						path: htdocs_prefix + '<config:min.dist.dest>',
+						href: '/<config:min.dist.dest>?{version}'
+					}
+				}
+			};
+		}
+
+		if (stylus || sweet) {
+			cfg.watch = {};
+			if (stylus) {
+				cfg.watch.stylus = {
+					files: 'styles/**',
+					tasks: 'stylus'
+				};
+			}
+			if (sweet) {
+				cfg.watch.sweet = {
+					files: ['content/**', 'templates/**'],
+					tasks: 'sweet'
+				};
+			}
+		}
+
+		if (sweet) {
+			cfg.server = {
+				port: 8000,
+				base: '<config:sweet.publish_dir>'
+			};
+		}
+
+		if (js) {
+			cfg.jshint = {
+				options: {
+					browser: true,
+					white: false,
+					smarttabs: true,
+					eqeqeq: true,
+					immed: true,
+					latedef: true,
+					newcap: true,
+					undef: true
+				},
+				globals: {
+					Modernizr: true
+				}
+			};
+			if (props.jquery) {
+				cfg.jshint.options.jquery = true;
+				cfg.jshint.globals.jQuery = true;
+			}
+
+			cfg.uglify = {};
+		}
+
+		props.config = jsToString(cfg, 2);
+
+		// Default task
+		var defaults = [];
+		if (stylus) defaults.push('stylus');
+		if (js) defaults.push('lint', 'concat', 'min');
+		if (sweet) defaults.push('sweet');
+		props.defaults = defaults.join(' ');
+
+		// NPM tasks
+		var npms = [];
+		if (stylus) npms.push('grunt-stylus');
+		if (sweet) npms.push('grunt-sweet');
+		props.npms = npms.join(' ');
 
 		// Files to copy (and process).
 		var files = init.filesToCopy(props);
@@ -82,3 +236,70 @@ exports.template = function(grunt, init, done) {
 	});
 
 };
+
+
+/* Utils */
+
+// Find the first `preferred` item existing in `arr`.
+function prefer(arr, preferred) {
+	for (var i = 0; i < preferred.length; i++) {
+		if (arr.indexOf(preferred[i]) !== -1) {
+			return preferred[i];
+		}
+	}
+	return null;
+}
+
+function jsToString(js, level, first) {
+	if (!level) level = 1;
+	if (first === undefined) first = true;
+	var array = (js.length !== undefined),
+		has = false,
+		s = array ? '[\n' : '{\n';
+
+	for (var key in js) {
+		var value = js[key],
+			type = typeof value;
+
+		if (!/^[a-z0-9_]+$/i.test(key)) key = "'" + key + "'";
+
+		s += stringCopy('\t', level);
+		if (!array) s += key + ': ';
+		switch (type) {
+			case "number":
+				s += value;
+				break;
+			case "boolean":
+				s += value ? 'true' : 'false';
+				break;
+			case "string":
+				s += "'" + value.replace(/'/g, '\\\'') + "'";
+				break;
+			case "array":
+			case "object":
+				s += jsToString(value, level + 1, false);
+				break;
+			default:
+				console.log("jsToString: unknown type: ", type);
+		}
+		if (type !== 'array' && type !== 'object') s += ',\n';
+		has = true;
+	}
+
+	if (has) {
+		s = s.slice(0, -2) + '\n';
+	}
+	else {
+		return (array ? '[]' : '{}') + (first ? '' : ',\n');
+	}
+
+	return s + stringCopy('\t', level - 1) + (array ? ']' : '}') + (first ? '' : ',\n');
+}
+
+function stringCopy(s, num) {
+	var ss = '';
+	for (var i = 0; i < num; i++) {
+		ss += s;
+	}
+	return ss;
+}
--- grunt/tasks/init/gruntfile/root/grunt.js
+++ grunt/tasks/init/gruntfile/root/grunt.js
@@ -2,9 +2,9 @@
 How to build this project?
 
 1. Install Grunt:
-  npm install grunt -g{% if (stylus) { %}
+  npm install grunt -g{% if (npms) { %}
   mkdir node_modules
-  npm install grunt-stylus{% } %}
+  npm install {%= npms %}{% } %}
 
 2. Build:
   grunt
@@ -15,85 +15,12 @@ module.exports = function(grunt) {
 	'use strict';
 
 	// Project configuration
-	grunt.initConfig({{% if (js) { if (package_json) { %}
-		pkg: '<json:package.json>',
-		meta: {
-			banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-				'<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-				'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-				' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-		},{% } else { %}
-		meta: {{% if (library) { %}{% if (!package_json) { %}
-			version: '0.0.0',
-			{% } %}banner: "/*! {%= library %} v{%= package_json ? '<%= pkg.version %>' : '<%= meta.version %>' %} - " +
-					"© {%= author_name %}, {%= author_url %}, <%= grunt.template.today('yyyy') %> - " +
-					"Licensed MIT */"{% } else { %}
-			banner: "/*! Author: {%= author_name %}, {%= author_url %}, <%= grunt.template.today('yyyy') %> */"{% } %}
-		},{% } } %}{% if (js) { %}
-		lint: {
-			files: [
-				'grunt.js',
-				'js/*.js',
-				'{%= lib_dir %}/**/*.js'
-			]
-		},
-		concat: {
-			dist: {
-				src: [
-					'js/utils.js',
-					'js/main.js'
-				],
-				dest: 'build/{%= file_name %}.js'
-			}
-		},
-		min: {
-			dist: {
-				src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-				dest: 'build/{%= file_name %}.min.js'
-			}
-		},{% } %}{% if (stylus) { %}
-		stylus: {
-			compile: {
-				files: {
-					'{%= wordpress ? "style.css" : "build/styles.css" %}': 'styles/index.styl'
-				},
-				options: {
-					'compress': true,
-					'include css': true,
-					'paths': ['styles']
-				}
-			}
-		},{% } %}{% if (stylus) { %}
-		watch: {{% if (stylus) { %}
-			stylus: {
-				files: 'styles/**',
-				tasks: 'stylus'
-			}{% } %}
-		},{% } %}{% if (js) { %}
-		jshint: {
-			options: {
-				browser: true,
-				white: false,
-				smarttabs: true,
-				eqeqeq: true,
-				immed: true,
-				latedef: true,
-				newcap: true,
-				undef: true{% if (jquery) { %},
-				jquery: true{% } %}
-			},
-			globals: {
-				Modernizr: true{% if (jquery) { %},
-				jQuery: true
-			{% } %}}
-		},
-		uglify: {}{% } %}
-	});
+	grunt.initConfig({%= config %});
 	{% if (stylus) { %}
-	grunt.loadNpmTasks('grunt-stylus');{% } %}
+	grunt.loadNpmTasks('grunt-stylus');{% } %}{% if (sweet) { %}
+	grunt.loadNpmTasks('grunt-sweet');{% } %}
 
 	// Default task
-	grunt.registerTask('default', '{%= stylus ? " stylus" : "" %}{%= js ? " lint concat min" : "" %}');
-
+	grunt.registerTask('default', '{%= defaults %}');{% if (sweet) { %}
+	grunt.registerTask('serve', 'server watch');{% } %}
 };
