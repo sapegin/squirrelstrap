1bafca4e342acb028a2eabc14544a7c2436e9654
Artem Sapegin <artem@sapegin.ru>
2012-10-17 23:40:26 +0400

    jQuery library template.

---
+++ grunt/tasks/init/jquerylib/root/name.js
@@ -0,0 +1,45 @@
+/**
+ * {%= name %}
+ *
+ * Makes bla bla.
+ *
+ * @version 0.0.0
+ * @requires jQuery
+ * @author {%= author_name %}
+ * @copyright {%= grunt.template.today('yyyy') %} {%= author_name %}, {%= author_url %}
+ * @license MIT
+ */
+
+/*jshint browser:true, jquery:true, white:false, smarttabs:true, eqeqeq:true,
+         immed:true, latedef:true, newcap:true, undef:true */
+/*global define:false */
+(function(factory) {  // Try to register as an anonymous AMD module
+	if (typeof define === 'function' && define.amd) {
+		define(['jquery'], factory);
+	} else {
+		factory(jQuery);
+	}
+}(function($) {
+	'use strict';
+
+	function {%= cls %}(container, options) {
+		options = $.extend({}, {%= cls %}.defaults, options);
+
+		this.container = container;
+		this.options = options;
+
+		this.init();
+	}
+
+	{%= cls %}.defaults = {
+	};
+
+	{%= cls %}.prototype = {
+		init: function() {
+			/* Magic begins here */
+		}
+	};
+
+	window.{%= cls %} = {%= cls %};
+
+}));
