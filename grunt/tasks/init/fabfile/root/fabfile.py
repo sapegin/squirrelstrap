2dfef71459a5dc4ba41cf9e791c107f718887ccf
Artem Sapegin <artem@sapegin.ru>
2012-11-10 01:13:36 +0400

    Fabfile template.

---
+++ grunt/tasks/init/fabfile/root/fabfile.py
@@ -0,0 +1,52 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.files import exists
+
+
+env.use_ssh_config = True
+env.hosts = ['{%= server %}']
+REPO = 'git/{%= name %}.git'
+DEST = 'projects/{%= name %}'
+
+
+@task(default=True)
+def deploy():
+	with cd(DEST):
+		run('git checkout master')
+		run('git pull')
+{% if (upgrade) { %}
+
+@task
+def upgrade():
+	with cd(DEST):
+		run('git checkout master')
+		run('git pull'){% if (grunt) { %}
+		run('npm update npm -g')
+		run('npm update grunt -g')
+		run('npm update'){% } %}{% } %}
+
+
+@task
+def setup():
+	setup_bare()
+	setup_clone(){% if (upgrade) { %}
+	upgrade(){% } %}
+{% if (bare) { %}
+
+def setup_bare():
+	if exists(REPO + '/config'):
+		return
+	run('mkdir -p %s' % REPO)
+	with cd(REPO):
+		run('git --bare init')
+		run('git config core.sharedrepository 1')
+		run('git config receive.denyNonFastforwards true')
+	remote = 'ssh://%s@%s/~/%s' % (env['user'], env['host'], REPO)
+	local('git remote add origin %s' % remote)
+	local('git push -u origin master')
+
+
+def setup_clone():
+	run('mkdir -p %s' % DEST)
+	with cd(DEST):
+		run('git clone ~/%s .' % REPO){% } %}
