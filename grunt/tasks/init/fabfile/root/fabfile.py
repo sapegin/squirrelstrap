9f8857d8d1b56be897c1ce643c27ed0498a2fb49
Artem Sapegin <artem@sapegin.ru>
2012-12-13 00:03:49 +0400

    Fabfile: add GitHub and BitBucket support.

--- grunt/tasks/init/fabfile/root/fabfile.py
+++ grunt/tasks/init/fabfile/root/fabfile.py
@@ -5,16 +5,20 @@ from fabric.contrib.files import exists
 
 env.use_ssh_config = True
 env.hosts = ['{%= server %}']
-REPO = 'git/{%= name %}.git'
+REPO = '{%= repo %}'
 DEST = 'projects/{%= name %}'
 
 
 @task(default=True)
 def deploy():
-	with cd(DEST):
-		run('git checkout master')
-		run('git pull')
+	if exists(DEST):
+		with cd(DEST):
+			run('git checkout master')
+			run('git pull')
+	else:
+		setup()
 {% if (upgrade) { %}
+
 
 @task
 def upgrade():
@@ -27,13 +31,7 @@ def upgrade():
 
 
 @task
-def setup():
-	setup_bare()
-	setup_clone(){% if (upgrade) { %}
-	upgrade(){% } %}
-{% if (bare) { %}
-
-def setup_bare():
+def setup():{% if (kind === 'bare') { %}
 	if exists(REPO + '/config'):
 		return
 	run('mkdir -p %s' % REPO)
@@ -43,10 +41,7 @@ def setup_bare():
 		run('git config receive.denyNonFastforwards true')
 	remote = 'ssh://%s@%s/~/%s' % (env['user'], env['host'], REPO)
 	local('git remote add origin %s' % remote)
-	local('git push -u origin master')
-
-
-def setup_clone():
-	run('mkdir -p %s' % DEST)
+	local('git push -u origin master'){% } %}
 	with cd(DEST):
-		run('git clone ~/%s .' % REPO){% } %}
+		run('git clone %s .' % REPO){% if (upgrade) { %}
+	upgrade(){% } %}
