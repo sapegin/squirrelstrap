83b9af7915df63b92bbed391c44f46a26baa21c5
Artem Sapegin <artem@sapegin.ru>
2013-03-10 21:12:13 +0400

    Squirrel.styl.

--- templates/stylusdir/template.js
+++ templates/stylusdir/template.js
@@ -9,47 +9,8 @@ exports.warnOn = 'styles';
 // The actual init template.
 exports.template = function(grunt, init, done) {
 
-	init.process({}, [
-			{
-				name: 'maxwidth',
-				message: 'Page max width',
-				default: '1000px',
-				warning: "CSS size or 'none'"
-			},
-			{
-				name: 'footer',
-				message: 'Sticky footer height',
-				default: 'none',
-				warning: "CSS size or 'none'"
-			},
-			{
-				name: 'linkDecor',
-				message: 'Link decoration',
-				default: 'underline',
-				warning: 'underline, gradient or border'
-			},
-			{
-				name: 'gridCols',
-				message: "Grid: number of columns or 'simple' or 'none'",
-				default: '12',
-				warning: "Number or 'none'"
-			},
-			{
-				name: 'gridGutter',
-				message: 'Grid: gutter width',
-				default: function(value, data, done) {
-					done(null, ['simple', 'none'].indexOf(data.gridCols) === -1 ? 5 : 'none');
-				},
-				warning: '0—19'
-			}
-		], function(err, props) {
+	init.process({}, [], function(err, props) {
 		grunt.util._.defaults(props, init.defaults);
-
-		// Grid
-		if (props.gridCols) {
-			var anygrid = require('../_src/anygrid');
-			props.gridCss = anygrid.generate(props.gridCols, props.gridGutter);
-		}
 
 		// Files to copy (and process).
 		var files = init.filesToCopy(props);
